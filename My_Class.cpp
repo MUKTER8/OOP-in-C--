ef2fa3d601e8e9c829c772e25eb02eff2d9d898a
MUKTER HOSAIN <mokterprodhan8@gmail.com>
2023-12-03 23:48:18 +0600

    changes

--- My_Class.cpp
+++ My_Class.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+class Student{
+public:
+      int id;
+      double gpa;
+};
 int main(){
-    
+Student Alim,Mukter;
+Alim.id = 101;
+Alim.gpa = 3.14;
+cout<<Alim.id<<"  "<<Alim.gpa<<endl;
+
+
+Mukter.id =388;
+Mukter.gpa = 3.94;
+cout<<Mukter.id<<"  "<<Mukter.gpa;
+
 }
